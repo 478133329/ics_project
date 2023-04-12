d95446a89bd269d4ae9e2c891f2ca78298921a1a
tracer-ics2022 <tracer@njuics.org>
2023-04-13 00:18:38 +0800

    feat: add instruction lui, auipc, jal, jalr, addi. 22051363 wangsijie Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:18:38 up 2 days,  4:27,  1 user,  load average: 0.26, 0.27, 0.22
    >  run NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     00:18:40 up 2 days,  4:27,  1 user,  load average: 0.26, 0.27, 0.22
    
    >  compile NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     00:24:30 up 2 days,  4:33,  1 user,  load average: 0.34, 0.27, 0.22
    
    >  run NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     00:24:32 up 2 days,  4:33,  1 user,  load average: 0.47, 0.30, 0.23
    
    >  compile NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     00:27:21 up 2 days,  4:36,  1 user,  load average: 0.27, 0.26, 0.23
    
    >  run NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     00:27:23 up 2 days,  4:36,  1 user,  load average: 0.27, 0.26, 0.23
    
    >  compile NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     00:31:35 up 2 days,  4:40,  1 user,  load average: 0.25, 0.22, 0.22
    
    >  run NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     00:31:37 up 2 days,  4:40,  1 user,  load average: 0.31, 0.24, 0.22
    
    >  compile NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     00:34:33 up 2 days,  4:43,  1 user,  load average: 0.40, 0.26, 0.23
    
    >  run NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     00:34:34 up 2 days,  4:43,  1 user,  load average: 0.40, 0.26, 0.23

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -25,6 +25,7 @@
 enum {
   TYPE_I, TYPE_U, TYPE_S,
   TYPE_N, // none
+  TYPE_J, TYPE_B, TYPE_R,
 };
 
 #define src1R() do { *src1 = R(rs1); } while (0)
@@ -32,6 +33,8 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i, 19, 12) << 12) | (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1) | 0; } while(0)
+#define immB() do { *imm = (SEXT(BITS(i, 31, 31) ,1) << 12) | (BITS(i, 30, 25) << 5) | (BITS(i ,11, 8) << 1) | (BITS(i, 7, 7) << 11); } while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -42,6 +45,9 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
+    case TYPE_J: 				   immJ(); break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
+    case TYPE_R: src1R(); src2R(); 		   break;
   }
 }
 
@@ -57,9 +63,15 @@ static int decode_exec(Decode *s) {
 }
 
   INSTPAT_START();
+  INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(rd) = imm);
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(rd) = s->pc + 4; s->dnpc = s->pc + imm);
+  INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, R(5) = s->pc + 4; s->dnpc = src1 + imm; R(rd) = R(5)); // R(5) is $t0
+
   INSTPAT("??????? ????? ????? 011 ????? 00000 11", ld     , I, R(rd) = Mr(src1 + imm, 8));
   INSTPAT("??????? ????? ????? 011 ????? 01000 11", sd     , S, Mw(src1 + imm, 8, src2));
+
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(rd) = src1 + imm);
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
