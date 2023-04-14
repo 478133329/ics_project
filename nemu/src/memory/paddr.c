5b28e0e1394dd1cafc308d73a407e0d16b537dd9
tracer-ics2022 <tracer@njuics.org>
2023-04-14 19:36:46 +0800

    feat: implement stdio.c string.c and add memory tracer. 22051363 wangsijie Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:36:46 up 2 days, 19:05,  1 user,  load average: 0.47, 0.34, 0.24
    >  run NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     19:36:52 up 2 days, 19:05,  1 user,  load average: 0.63, 0.38, 0.25
    
    >  compile NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     19:37:26 up 2 days, 19:06,  1 user,  load average: 0.38, 0.34, 0.24
    
    >  run NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     19:37:32 up 2 days, 19:06,  1 user,  load average: 0.63, 0.40, 0.26
    
    >  compile NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     20:02:10 up 2 days, 19:31,  1 user,  load average: 0.15, 0.20, 0.19
    
    >  run NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     20:02:16 up 2 days, 19:31,  1 user,  load average: 0.22, 0.21, 0.19
    
    >  compile NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     20:06:33 up 2 days, 19:35,  1 user,  load average: 0.44, 0.25, 0.20
    
    >  run NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     20:06:39 up 2 days, 19:35,  1 user,  load average: 0.56, 0.28, 0.21
    
    >  compile NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     20:07:04 up 2 days, 19:36,  1 user,  load average: 0.70, 0.34, 0.23
    
    >  run NEMU
    22051363 wangsijie
    Linux wangsijie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux
     20:07:10 up 2 days, 19:36,  1 user,  load average: 0.72, 0.35, 0.24

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -57,6 +57,12 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
+  #ifdef CONFIG_MTRACE
+    if (likely(in_pmem(addr))) {
+        word_t w = pmem_read(addr, len);
+        Log(" Read  from memory at %08x for %d bytes for %016lx.", addr, len, w);
+    }
+  #endif
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -64,6 +70,11 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+  #ifdef CONFIG_MTRACE
+    if (likely(in_pmem(addr))) { 
+        Log("Write %016lx to memory at %08x for %d bytes.", data, addr, len);
+    }
+  #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
