4dbd24038b4ec5b43267799ba74e9122cea1ff8a
tracer-ics2022 <tracer@njuics.org>
2023-06-15 23:33:42 +0800

    >  compile NEMU 22051363 wangsijie Linux wangsijie-virtual-machine 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:33:42 up 3 days,  5:13,  1 user,  load average: 0.79, 0.36, 0.36

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -33,11 +33,11 @@ void init_proc() {
   //naive_uload(NULL, "/bin/timer-test");
   //naive_uload(NULL, "/bin/event-test");
   //naive_uload(NULL, "/bin/bmp-test");
-  naive_uload(NULL, "/bin/nslider");
+  //naive_uload(NULL, "/bin/nslider");
   //naive_uload(NULL, "/bin/menu");
   //naive_uload(NULL, "/bin/nterm");
   //naive_uload(NULL, "/bin/bird");
-  //naive_uload(NULL, "/bin/pal");
+  naive_uload(NULL, "/bin/pal");
 
 }
 
